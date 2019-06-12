579def449f7c59b416eb38112af5a57b088a2392
Vojtech Rinik <vojto@rinik.net>
2019-06-12 07:28:01 +0200

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+plugins: [
+    resolve(),
+    commonjs({
+      namedExports: {
+        "node_modules/react-sizeme/dist/react-sizeme.js": ["SizeMe"],
+      }
+    }),
+    typescript({
+      typescript: require('typescript'),
+      exclude: [],
+    }),
+    postcss({
+      modules: true,
+      namedExports: true
+    })
+  ],
+}
